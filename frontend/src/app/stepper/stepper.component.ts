33cfbacea6c17a83461fd771c403f1762f7c6519
marvinalnashi <marvinalnashiedu@gmail.com>
2023-12-13 17:01:22 +0100

    Added frames for dynamic item selection and description view
    - Added left frame for each of the three selectable categories, which contains the items
    - Added right frame that contains the description of the hovered item in the left pane for each of the three selectable categories
    - When an item is clicked the state of the description gets 'pinned', so that it stops changing when hovering over other items
    - Imported CommonModule as a standalone component
    - Added items and their descriptions in arrays, as custom added items do not need to be saved

--- frontend/src/app/stepper/stepper.component.ts
+++ frontend/src/app/stepper/stepper.component.ts
@@ -1,19 +1,71 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, AfterViewInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-stepper',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './stepper.component.html',
   styleUrl: './stepper.component.css'
 })
-export class StepperComponent implements OnInit{
-  constructor() { }
+export class StepperComponent implements OnInit, AfterViewInit {
+  algorithms = [
+    { name: 'Algorithm 1', description: 'Description of Algorithm 1' },
+    { name: 'Algorithm 2', description: 'Description of Algorithm 2' },
+    { name: 'Algorithm 3', description: 'Description of Algorithm 3' },
+  ];
+
+  pricingmodels = [
+    { name: 'Pricing model 1', description: 'Description of Pricing model 1' },
+    { name: 'Pricing model 2', description: 'Description of Pricing model 2' },
+    { name: 'Pricing model 3', description: 'Description of Pricing model 3' },
+  ];
+
+  twinworlds = [
+    { name: 'Twin world 1', description: 'Description of Twin world 1' },
+    { name: 'Twin world 2', description: 'Description of Twin world 2' },
+    { name: 'Twin world 3', description: 'Description of Twin world 3' },
+  ];
+
+  currentDescription: string = '';
+  pinnedDescription: string | null = null;
+
+  selectedAlgorithm: any = null;
+  selectedPricingModel: any = null;
+  selectedTwinWorld: any = null;
+
+  constructor(private elementRef: ElementRef) { }
 
   ngOnInit(): void {
+
+  }
+
+  changeDescription(description: string) {
+    if (this.pinnedDescription === null) {
+      this.currentDescription = description;
+    }
+  }
+
+  pinDescription(description: string) {
+    this.pinnedDescription = description;
+    this.currentDescription = description;
+  }
+
+  resetDescriptions() {
+    this.currentDescription = '';
+    this.pinnedDescription = null;
+  }
+
+  ngAfterViewInit() {
+    const self = this;
+
+    this.elementRef.nativeElement.ownerDocument
+      .body.style.backgroundColor = '#1e1e2d';
+
     $(".step").click(function() {
       $(this).addClass("active").prevAll().addClass("active");
       $(this).nextAll().removeClass("active");
+      self.resetDescriptions();
     });
 
     $(".step01").click(function() {
