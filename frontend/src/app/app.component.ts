828680d17c51b8509edd6dd6cd5337c310c98b65
marvinalnashi <marvinalnashiedu@gmail.com>
2023-12-13 13:04:02 +0100

    Added full Bootstrap support
    - Fully implemented Bootstrap and ng-bootstrap libraries
    - Added test page that demonstrates that Bootstrap is working as intended

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterOutlet } from "@angular/router";
+import {NgbInputDatepicker, NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NgbInputDatepicker],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.css",
 })
 export class AppComponent {
   title = "frontend";
+  constructor(private modalService: NgbModal) {
+  }
+
+  public open(modal: any): void {
+    this.modalService.open(modal);
+  }
 }
