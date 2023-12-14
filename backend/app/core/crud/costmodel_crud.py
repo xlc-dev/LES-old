0afba6e0494971113a935ecfc755265509caab75
xlc-dev <>
2023-12-14 11:29:56 +0100

    add cost model routers

---
+++ backend/app/core/crud/costmodel_crud.py
@@ -0,0 +1,15 @@
+from sqlmodel import Session, select
+
+from app.core.crud.base import CRUDBase
+from app.core.models.costmodel_model import CostModel
+from app.core.schemas.costmodel_schema import CostModelCreate, CostModelUpdate
+
+
+class CRUDCostModel(CRUDBase[CostModel, CostModelCreate, CostModelUpdate]):
+    def get_by_name(self, *, session: Session, name: str):
+        return session.exec(
+            select(CostModel).where(CostModel.name == name)
+        ).first()
+
+
+costmodel_crud = CRUDCostModel(CostModel)
--- backend/app/core/models/costmodel_model.py
+++ backend/app/core/models/costmodel_model.py
@@ -1,9 +1,15 @@
-from sqlmodel import Field
+from typing import Optional
 
-from app.core.models.base_model import BaseModel
+from sqlmodel import SQLModel, Field
 
 
-class CostModel(BaseModel, table=True):
+class CostModel(SQLModel, table=True):
+    id: int = Field(primary_key=True)
     name: str = Field(index=True, unique=True, nullable=False)
-    buy_price: int = Field(nullable=False)
-    sell_price: int = Field(nullable=False)
+    description: str = Field(nullable=False)
+    price_network_buy_consumer: float = Field(nullable=False)
+    price_network_sell_consumer: float = Field(nullable=False)
+    fixed_division: Optional[float] = Field(nullable=True)
+    stock_time_delta: Optional[int] = Field(nullable=True)
+    algo_1: str = Field(nullable=False)
+    algo_2: Optional[str] = Field(nullable=True)
