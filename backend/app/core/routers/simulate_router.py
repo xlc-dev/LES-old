02d8abb4ddaae2757baff75f998d215a0cb50e2b
xlc-dev <>
2023-12-14 11:30:27 +0100

    add simulation router

--- backend/app/core/routers/simulate_router.py
+++ backend/app/core/routers/simulate_router.py
@@ -1,11 +1,29 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
+from sqlmodel import Session
+
+from app.utils import get_session
+
+from app.core.schemas import simulation_schema
+
+from app.core.routers.twinworld_router import get_twinworlds
+from app.core.routers.costmodel_router import get_costmodels
 
 router = APIRouter()
 
 
-@router.get("/start")
-def start():
+@router.get("/load-data", response_model=simulation_schema.SimulationData)
+async def get_data(*, session: Session = Depends(get_session)):
+    twinworlds = await get_twinworlds(session=session)
+    costmodels = await get_costmodels(session=session)
+
+    return simulation_schema.SimulationData(
+        twin_world=twinworlds, cost_model=costmodels
+    )
+
+
+@router.post("/start")
+async def start(*, session: Session = Depends(get_session)):
     return {"message": "Simulation started"}
 
 
