2a1f48501e73b86638dd2f7118c84d30ce31f756
xlc-dev <>
2023-12-14 11:30:23 +0100

    cleanup models

--- backend/app/core/models/household_model.py
+++ backend/app/core/models/household_model.py
@@ -1,8 +1,6 @@
 from typing import TYPE_CHECKING
 
-from sqlmodel import Field, Relationship
-
-from app.core.models.base_model import BaseModel
+from sqlmodel import SQLModel, Field, Relationship
 
 
 if TYPE_CHECKING:
@@ -10,14 +8,13 @@ if TYPE_CHECKING:
     from app.core.models.twinworld_model import TwinWorld
 
 
-class Household(BaseModel, table=True):
-    "Table for households"
-
+class Household(SQLModel, table=True):
+    id: int = Field(primary_key=True)
     name: str = Field(index=True, unique=True, nullable=False)
     size: int = Field(default=1, nullable=False)
     energy_usage: int = Field(nullable=False)
     solar_panels: int = Field(default=0, nullable=False)
-    solar_yield: int
+    solar_yield_yearly: int
 
     twinworld_id: int = Field(foreign_key="twinworld.id")
     twinworld: "TwinWorld" = Relationship(back_populates="household")
