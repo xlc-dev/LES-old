bd1a6d54c400f1b53881f5575a3cb156b5129bad
xlc-dev <>
2023-12-14 19:59:05 +0100

    restructure

--- backend/app/app.py
+++ backend/app/app.py
@@ -54,7 +54,7 @@ def create_app() -> FastAPI:
     app = FastAPI(
         title=settings.project_name,
         description=f"API for the {settings.project_name} app",
-        version="0.1",
+        version="1.0",
         openapi_tags=tags_metadata,
         openapi_url=settings.openapi_url if settings.development else None,
         redoc_url=None,
@@ -62,19 +62,40 @@ def create_app() -> FastAPI:
         dependencies=[Depends(set_sec_headers)],
     )
 
-    # TODO: CORS
+    folder = os.path.join(Path().resolve(), "data/logs")
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
     if settings.development:
-        app.add_middleware(TrustedHostMiddleware, allowed_hosts=["localhost"])
+        logging.basicConfig(
+            filename=f"{folder}/FastAPI.log", level=logging.DEBUG
+        )
+
+        @app.on_event("startup")
+        def on_startup():
+            "Create DB and tables on startup"
+            create_db_and_tables()
+
+        @app.middleware("http")
+        async def add_process_time_header(request: Request, call_next):
+            start_time = time.time()
+            response = await call_next(request)
+            process_time = time.time() - start_time
+            response.headers["X-Process-Time"] = str(process_time)
+            return response
+
     else:
-        app.add_middleware(TrustedHostMiddleware, allowed_hosts=["localhost"])
+        logging.basicConfig(
+            filename=f"{folder}/FastAPI.log", level=logging.WARNING
+        )
 
-    origins = [settings.server_host]
-
+    # TODO: CORS
     # app.add_middleware(HTTPSRedirectMiddleware)
     app.add_middleware(GZipMiddleware)
+    app.add_middleware(TrustedHostMiddleware, allowed_hosts=["localhost"])
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=origins,
+        allow_origins=[settings.server_host],
         allow_credentials=True,
         allow_methods=["GET", "POST", "PATCH", "DELETE"],
         allow_headers=[
@@ -86,29 +107,6 @@ def create_app() -> FastAPI:
         ],
         expose_headers=[],
     )
-
-    if settings.development:
-
-        @app.middleware("http")
-        async def add_process_time_header(request: Request, call_next):
-            start_time = time.time()
-            response = await call_next(request)
-            process_time = time.time() - start_time
-            response.headers["X-Process-Time"] = str(process_time)
-            return response
-
-    folder = os.path.join(Path().resolve(), "data/logs")
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-
-    if settings.development:
-        logging.basicConfig(
-            filename=f"{folder}/FastAPI.log", level=logging.DEBUG
-        )
-    else:
-        logging.basicConfig(
-            filename=f"{folder}/FastAPI.log", level=logging.WARNING
-        )
 
     app.include_router(
         simulation_router.router,
@@ -134,10 +132,5 @@ def create_app() -> FastAPI:
             prefix=f"{settings.api_prefix}/seed",
             tags=["Seed"],
         )
-
-    @app.on_event("startup")
-    def on_startup():
-        "Create DB and tables on startup"
-        create_db_and_tables()
 
     return app
