737ac939c1b9b1109c642f9578de57591b486d95
xlc-dev <>
2023-12-14 11:18:28 +0100

    add initial schemas

---
+++ backend/app/core/schemas/costmodel_schema.py
@@ -0,0 +1,13 @@
+from app.core.models.costmodel_model import CostModel
+
+
+class CostModelRead(CostModel):
+    pass
+
+
+class CostModelCreate(CostModel):
+    pass
+
+
+class CostModelUpdate(CostModel):
+    pass
