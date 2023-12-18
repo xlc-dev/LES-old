060498f28b8bcd2dc0dd93e8cf6eef31e5530ad5
marvinalnashi <marvinalnashiedu@gmail.com>
2023-12-18 01:58:33 +0100

    Seperated Sidebar to its' own component

--- frontend/src/components/baseLayout.svelte
+++ frontend/src/components/baseLayout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import {stepperData, twdata} from "../lib/stores";
+  import Sidebar from "../partials/Sidebar.svelte";
+  
   console.log($stepperData);
 
   function handleButtonClick(action) {
@@ -15,22 +17,7 @@
 <!--  </div>-->
 
 <div class="flex">
-  <div class="w-64 bg-les-bg h-screen p-4">
-    <div class="flex flex-col">
-      <button class="mb-2 p-2 bg-blue-500 text-white" on:click={() => handleButtonClick('Action1')}>Dashboard</button>
-      <button class="mb-2 p-2 bg-blue-500 text-white" on:click={() => handleButtonClick('Action2')}>Simulation</button>
-      <button class="mb-2 p-2 bg-blue-500 text-white" on:click={() => handleButtonClick('Action3')}>Schedulable loads</button>
-      <button class="mb-2 p-2 bg-blue-500 text-white" on:click={() => handleButtonClick('Action4')}>Stock market</button>
-    </div>
-    <div class="flex flex-col">
-    </div>
-    <div class="mt-auto fixed bottom-0">
-      <h3 class="text-lg font-semibold text-white">Selected Options:</h3>
-      <p class="text-white">Twin World: {$twdata.twin_world || 'Not selected'}</p>
-      <p class="text-white">Cost Model: {$twdata.cost_model || 'Not selected'}</p>
-      <p class="text-white">Algorithm: {$twdata.algorithm || 'Not selected'}</p>
-    </div>
-  </div>
+  <Sidebar />
   <div class="flex-1 bg-les-frame">
   </div>
 </div>
