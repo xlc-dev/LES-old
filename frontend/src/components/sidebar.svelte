611c30e44cb5b5768ec45d9e5fcd319253106f96
xlc-dev <>
2023-12-18 11:20:26 +0100

    improve sidebar stying and make buttons work

--- frontend/src/components/sidebar.svelte
+++ frontend/src/components/sidebar.svelte
@@ -1,27 +1,37 @@
 <script lang="ts">
+  import { createEventDispatcher } from "svelte";
+
   import { twdata } from "../lib/stores";
 
+  const dispatch = createEventDispatcher();
+
   const handleButtonClick = (action) => {
-    console.log(`Button clicked: ${action}`);
+    dispatch("click", { action });
   };
 </script>
 
-<div class="w-64 h-screen p-4">
+<div class="fixed flex flex-col justify-between left-0 top-0 h-screen w-64 p-4 bg-gray-900">
   <div class="flex flex-col">
-    <button class="mb-2 p-2 bg-blue-500 text-white" on:click={() => handleButtonClick("Action1")}
+    <button class="mb-2 p-2 bg-blue-500 text-white" on:click={() => handleButtonClick("Dashboard")}
       >Dashboard</button>
-    <button class="mb-2 p-2 bg-blue-500 text-white" on:click={() => handleButtonClick("Action2")}
-      >Simulation</button>
-    <button class="mb-2 p-2 bg-blue-500 text-white" on:click={() => handleButtonClick("Action3")}
-      >Schedulable loads</button>
-    <button class="mb-2 p-2 bg-blue-500 text-white" on:click={() => handleButtonClick("Action4")}
-      >Stock market</button>
+    <button
+      class="mb-2 p-2 bg-blue-500 text-white"
+      on:click={() => handleButtonClick("Simulation")}>Simulation</button>
+    <button
+      class="mb-2 p-2 bg-blue-500 text-white"
+      on:click={() => handleButtonClick("SchedulableLoads")}>Schedulable loads</button>
+    <button
+      class="mb-2 p-2 bg-blue-500 text-white"
+      on:click={() => handleButtonClick("StockMarket")}>Stock market</button>
   </div>
-  <div class="flex flex-col"></div>
-  <div class="mt-auto fixed bottom-0">
-    <h3 class="text-lg font-semibold text-white">Selected Options:</h3>
-    <p class="text-white">Twin World: {$twdata.twin_world}</p>
-    <p class="text-white">Cost Model: {$twdata.cost_model}</p>
-    <p class="text-white">Algorithm: {$twdata.algorithm}</p>
+  <div>
+    <h3 class="text-lg font-semibold text-white pb-4">Selected Options:</h3>
+    <p class="text-gray-400">Twin World: {$twdata.twin_world}</p>
+    <p class="text-gray-400">Cost Model: {$twdata.cost_model}</p>
+    <p class="text-gray-400">Algorithm: {$twdata.algorithm}</p>
+    <hr class="border-gray-800 my-4" />
+    <button
+      class="mb-2 p-2 bg-blue-500 text-white w-full"
+      on:click={() => handleButtonClick("Stop")}>Stop</button>
   </div>
 </div>
