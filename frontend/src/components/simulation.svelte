cb348b5d0b834e4facc48a6a3fb3b1b5439a91d6
xlc-dev <>
2023-12-18 15:06:16 +0100

    add initial simulation page

--- frontend/src/components/simulation.svelte
+++ frontend/src/components/simulation.svelte
@@ -1,12 +1,29 @@
 <script lang="ts">
+  import type { HouseholdRead } from "../lib/client";
   import { stepperData } from "../lib/stores";
 
-  const showHome = () => {};
+  import HomeView from "./homeView.svelte";
+
+  let household: HouseholdRead;
+
+  $: selectedHousehold = household;
+
+  const showHome = (data: HouseholdRead) => {
+    household = data;
+  };
 </script>
 
-<div class="grid grid-cols-5 gap-4">
-  {#each $stepperData as data}
-    <button class="text-gray-800 cursor-pointer hover:text-blue-500" on:click={showHome}
-      >{data.name}</button>
-  {/each}
-</div>
+{#if !selectedHousehold}
+  <div class="grid grid-cols-5 gap-4">
+    {#each $stepperData as data}
+      <button
+        class="text-gray-800 cursor-pointer hover:text-blue-500 flex items-center gap-4"
+        on:click={() => showHome(data)}>
+        <img src="/house.svg" class="h-6" alt="" />
+        {data.name}
+      </button>
+    {/each}
+  </div>
+{:else}
+  <HomeView {household} />
+{/if}
