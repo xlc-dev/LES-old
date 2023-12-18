2dc4916bc3dcabc5c0d3cac5763d4af0cad1e5b6
xlc-dev <>
2023-12-18 15:06:44 +0100

    update tailwind colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{svelte,html}", "./index.html"],
+  content: ["./src/**/*.svelte", "./index.html"],
   theme: {
     extend: {
       colors: {
-        "les-gray": "#373737",
         "les-blue": "#1565c0",
         "les-red": "#f23f44",
+        "les-red-dark": "#610000",
         "les-bg": "#1e1e2d",
+        "les-bg-dark": "#12121B",
+        "les-highlight": "#2a2a3d",
         "les-frame": "#dadada",
       },
       transitionProperty: {
