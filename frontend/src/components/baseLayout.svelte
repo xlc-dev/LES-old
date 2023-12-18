63199a5b7ff4123631b8175aef94028cf4dc1770
xlc-dev <>
2023-12-18 11:20:34 +0100

    add animations and switching between component

--- frontend/src/components/baseLayout.svelte
+++ frontend/src/components/baseLayout.svelte
@@ -1,11 +1,44 @@
 <script lang="ts">
+  import { blur } from "svelte/transition";
+
   import Sidebar from "./sidebar.svelte";
   import SchedulableLoadTable from "./schedulableLoadTable.svelte";
+  import Simulation from "./simulation.svelte";
+
+  let selectedComponent = null;
+
+  const handleButtonClick = (action) => {
+    switch (action) {
+      case "Dashboard":
+        selectedComponent = null;
+        break;
+      case "Simulation":
+        selectedComponent = Simulation;
+        break;
+      case "SchedulableLoads":
+        selectedComponent = SchedulableLoadTable;
+        break;
+      case "StockMarket":
+        selectedComponent = null;
+        break;
+      case "Stop":
+        selectedComponent = null;
+        break;
+    }
+  };
 </script>
 
-<div class="flex">
-  <Sidebar />
-  <div class="flex-1 bg-les-frame">
+<Sidebar on:click={(e) => handleButtonClick(e.detail.action)} />
+<main class="flex-1 bg-les-frame ml-64 p-4 min-h-screen" in:blur>
+  {#if selectedComponent !== null}
+    <div in:blur>
+      {#key selectedComponent}
+        <div in:blur>
+          <svelte:component this={selectedComponent} />
+        </div>
+      {/key}
+    </div>
+  {:else}
     <SchedulableLoadTable />
-  </div>
-</div>
+  {/if}
+</main>
