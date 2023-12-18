a1a11bb381f629e14067c28db5ab26f318b2d551
xlc-dev <>
2023-12-18 15:05:35 +0100

    add stop logic, titles

--- frontend/src/components/baseLayout.svelte
+++ frontend/src/components/baseLayout.svelte
@@ -2,43 +2,56 @@
   import { blur } from "svelte/transition";
 
   import Sidebar from "./sidebar.svelte";
+  import TitleBar from "./titleBar.svelte";
+  import Result from "./result.svelte";
   import SchedulableLoadTable from "./schedulableLoadTable.svelte";
   import Simulation from "./simulation.svelte";
 
   let selectedComponent = null;
+  let title = "Dashboard";
+  let stop = false;
 
-  const handleButtonClick = (action) => {
+  const handleButtonClick = (action: string) => {
     switch (action) {
       case "Dashboard":
         selectedComponent = null;
+        title = "Dashboard";
         break;
       case "Simulation":
         selectedComponent = Simulation;
+        title = "Simulation";
         break;
       case "SchedulableLoads":
         selectedComponent = SchedulableLoadTable;
+        title = "Schedulable Loads";
         break;
       case "StockMarket":
         selectedComponent = null;
+        title = "Stock Market";
         break;
       case "Stop":
-        selectedComponent = null;
+        stop = true;
         break;
     }
   };
 </script>
 
-<Sidebar on:click={(e) => handleButtonClick(e.detail.action)} />
-<main class="flex-1 bg-les-frame ml-64 p-4 min-h-screen" in:blur>
-  {#if selectedComponent !== null}
-    <div in:blur>
-      {#key selectedComponent}
-        <div in:blur>
-          <svelte:component this={selectedComponent} />
-        </div>
-      {/key}
-    </div>
-  {:else}
-    <SchedulableLoadTable />
-  {/if}
-</main>
+{#if !stop}
+  <Sidebar on:click={(e) => handleButtonClick(e.detail.action)} />
+  <TitleBar {title} />
+  <main class="flex-1 bg-les-frame ml-64 p-4 min-h-screen" in:blur>
+    {#if selectedComponent !== null}
+      <div in:blur>
+        {#key selectedComponent}
+          <div in:blur>
+            <svelte:component this={selectedComponent} />
+          </div>
+        {/key}
+      </div>
+    {:else}
+      <SchedulableLoadTable />
+    {/if}
+  </main>
+{:else}
+  <Result />
+{/if}
