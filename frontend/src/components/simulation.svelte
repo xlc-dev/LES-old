e6741362dc640730b51ae2f4a0a9344bced12c89
xlc-dev <>
2023-12-18 11:20:18 +0100

    add simulation view

---
+++ frontend/src/components/simulation.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { stepperData } from "../lib/stores";
+
+  const showHome = () => {};
+</script>
+
+<div class="grid grid-cols-5 gap-4">
+  {#each $stepperData as data}
+    <button class="text-gray-800 cursor-pointer hover:text-blue-500" on:click={showHome}
+      >{data.name}</button>
+  {/each}
+</div>
