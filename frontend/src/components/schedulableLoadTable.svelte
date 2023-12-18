c58c1e0a1f33dc44b36321ab024a0f23b25834b1
marvinalnashi <marvinalnashiedu@gmail.com>
2023-12-18 17:29:31 +0100

    Added filter buttons to schedulable load table
    - Added filters in the schedulable load table header to sort the items of the rows by ascending and descending

--- frontend/src/components/schedulableLoadTable.svelte
+++ frontend/src/components/schedulableLoadTable.svelte
@@ -1,14 +1,24 @@
 <script lang="ts">
   import { stepperData } from "../lib/stores";
 
-  const formatAppliances = (appliances) => {
-    return appliances
-      .map(
-        (appliance) =>
-          `${appliance.name} (Power: ${appliance.power}, Duration: ${appliance.duration})`
-      )
-      .join(", ");
-  };
+  let sortColumn = null;
+  let sortOrder = 'asc';
+
+  function sortData(column) {
+    sortColumn = column;
+    stepperData.update(data => {
+      return data.slice().sort((a, b) => {
+        if (a[column] < b[column]) return sortOrder === 'asc' ? -1 : 1;
+        if (a[column] > b[column]) return sortOrder === 'asc' ? 1 : -1;
+        return 0;
+      });
+    });
+    sortOrder = sortOrder === 'asc' ? 'desc' : 'asc';
+  }
+
+  function formatAppliances(appliances) {
+    return appliances.map(appliance => `${appliance.name} (Power: ${appliance.power}, Duration: ${appliance.duration})`).join(', ');
+  }
 </script>
 
 <table class="min-w-full leading-normal rounded-lg overflow-hidden">
@@ -17,10 +27,12 @@
       <th
         class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
         ID
+        <button on:click={() => sortData('id')}>Sort</button>
       </th>
       <th
         class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
         Name
+        <button on:click={() => sortData('name')}>Sort</button>
       </th>
       <th
         class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
@@ -29,14 +41,17 @@
       <th
         class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
         Energy Usage
+        <button on:click={() => sortData('energy_usage')}>Sort</button>
       </th>
       <th
         class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
         Solar Panels
+        <button on:click={() => sortData('solar_panels')}>Sort</button>
       </th>
       <th
         class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
         Solar Yield Yearly
+        <button on:click={() => sortData('solar_yield_yearly')}>Sort</button>
       </th>
       <th
         class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
