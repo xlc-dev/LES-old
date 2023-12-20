d228649332f0ff63fb89796b290e4245daea997c
marvinalnashi <marvinalnashiedu@gmail.com>
2023-12-20 01:55:27 +0100

    Added sort button for size in Schedulable Load Table

--- frontend/src/components/schedulableLoadTable.svelte
+++ frontend/src/components/schedulableLoadTable.svelte
@@ -129,6 +129,7 @@
       <th
         class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs text-gray-600 uppercase tracking-wider">
         Size
+        <button on:click={() => sortData('size')}>Sort</button>
       </th>
       <th
         class="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs text-gray-600 uppercase tracking-wider">
