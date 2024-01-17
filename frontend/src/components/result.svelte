89a870d47613a12108d3f33704650f621b422038
Marvin Al Nashi <marvinalnashiedu@gmail.com>
2024-01-17 13:08:33 +0100

    Added basic interface for result component
    - The end view now contains a window with a graph, a button to refresh the application, and a button that will be used for downloading logs in the future
    - TODO: add functionality to download button and fix chart.js graphs

--- frontend/src/components/result.svelte
+++ frontend/src/components/result.svelte
@@ -6,6 +6,65 @@
   the application has gathered during the session and provides the researcher a way to display
   and download this data before a new session is started.
   */
+
+  import { ApplianceDays, ApplianceType } from "../lib/client";
+  import { Chart } from "svelte-chartjs";
+  import "chart.js/auto";
+
+  let data = {
+    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  let options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  function refreshPage() {
+    location.reload();
+  }
+
+  function doNothing() {
+  }
 </script>
 
-<h1>Result</h1>
+<div class="max-w-3xl mx-auto pt-8">
+  <div class="mt-8 bg-white rounded-lg p-4 mb-8 border-4 border-gray-400 shadow">
+    <Chart type="bar" {data} {options} />
+    <div class="buttons">
+      <button on:click={refreshPage}>Refresh Page</button>
+      <button on:click={doNothing}>Do Nothing</button>
+    </div>
+    <Chart type="bar" {data} {options} />
+    <div class="buttons">
+      <button on:click={refreshPage}>Refresh Page</button>
+      <button on:click={doNothing}>Do Nothing</button>
+    </div>
+  </div>
+</div>
