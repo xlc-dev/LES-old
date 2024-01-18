2ec8f736cd3a989546ba36c7c3608fb22afe7b9f
xlc-dev <>
2024-01-18 18:22:00 +0100

    add queries

--- backend/app/core/crud/energyflow_crud.py
+++ backend/app/core/crud/energyflow_crud.py
@@ -1,5 +1,5 @@
 from sqlmodel import Session, select
-from sqlalchemy import func, desc
+from sqlalchemy import func, desc, asc
 
 from app.core.crud.base import CRUDBase
 from app.core.models.energyflow_model import (
@@ -15,15 +15,16 @@ class CRUDEnergyFlow(CRUDBase[EnergyFlow, EnergyFlowCreate, EnergyFlowUpdate]):
             select(EnergyFlow).where(EnergyFlow.timestamp == timestamp)
         ).first()
 
-    def get_by_solar_produced(self, *, session: Session):
+    def get_by_solar_produced(
+        self, *, session: Session, limit: int = 0, offset: int = 0
+    ):
         return session.exec(
             select(EnergyFlow)
             .where(EnergyFlow.solar_produced > 0)
             .order_by(desc(EnergyFlow.solar_produced))  # type: ignore
+            .limit(limit)
+            .offset(offset)
         ).all()
-
-    def get_by_solar_produced_sim(self, *, session: Session):
-        return session.exec(select(EnergyFlow)).all()
 
     def get_start_end_date(self, *, session: Session):
         min_timestamp_query = select(EnergyFlow).filter(
@@ -38,6 +39,21 @@ class CRUDEnergyFlow(CRUDBase[EnergyFlow, EnergyFlowCreate, EnergyFlowUpdate]):
         results_min_timestamp = session.exec(min_timestamp_query).first()
         results_max_timestamp = session.exec(max_timestamp_query).first()
         return results_min_timestamp, results_max_timestamp
+
+    def get_all_sorted_by_timestamp(
+        self, *, session: Session, limit: int = 0, offset: int = 0
+    ):
+        return session.exec(
+            select(EnergyFlow)
+            .order_by(asc(EnergyFlow.timestamp))
+            .limit(limit)
+            .offset(offset)
+        ).all()
+
+    def get_sorted_by_timestamp(self, *, session: Session):
+        return session.exec(
+            select(EnergyFlow).order_by(asc(EnergyFlow.timestamp))
+        ).first()
 
 
 energyflow_crud = CRUDEnergyFlow(EnergyFlow)
