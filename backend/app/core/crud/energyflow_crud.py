a4f512783b5fd720bc30167c3a32e1d744ed2ac4
xlc-dev <>
2024-01-19 15:03:54 +0100

    fix db queries

--- backend/app/core/crud/energyflow_crud.py
+++ backend/app/core/crud/energyflow_crud.py
@@ -1,5 +1,5 @@
 from sqlmodel import Session, select
-from sqlalchemy import func, desc, asc
+from sqlalchemy import func
 
 from app.core.crud.base import CRUDBase
 from app.core.models.energyflow_model import (
@@ -16,23 +16,33 @@ class CRUDEnergyFlow(CRUDBase[EnergyFlow, EnergyFlowCreate, EnergyFlowUpdate]):
         ).first()
 
     def get_by_solar_produced(
-        self, *, session: Session, limit: int = 0, offset: int = 0
+        self, *, session: Session, limit: int = 10000, offset: int = 0
     ):
         return session.exec(
             select(EnergyFlow)
             .where(EnergyFlow.solar_produced > 0)
-            .order_by(desc(EnergyFlow.solar_produced))  # type: ignore
+            .where(EnergyFlow.id >= offset)
+            .where(EnergyFlow.id < offset + limit)
+            .order_by(EnergyFlow.solar_produced.desc())
+        ).all()
+
+    def get_all_sorted_by_timestamp(
+        self, *, session: Session, limit: int = 10000, offset: int = 0
+    ):
+        return session.exec(
+            select(EnergyFlow)
+            .order_by(EnergyFlow.timestamp.asc())
             .limit(limit)
             .offset(offset)
         ).all()
 
     def get_start_end_date(self, *, session: Session):
         min_timestamp_query = select(EnergyFlow).filter(
-            EnergyFlow.timestamp  # type: ignore
+            EnergyFlow.timestamp
             == session.execute(select(func.min(EnergyFlow.timestamp))).scalar()
         )
         max_timestamp_query = select(EnergyFlow).filter(
-            EnergyFlow.timestamp  # type: ignore
+            EnergyFlow.timestamp
             == session.execute(select(func.max(EnergyFlow.timestamp))).scalar()
         )
 
@@ -40,19 +50,9 @@ class CRUDEnergyFlow(CRUDBase[EnergyFlow, EnergyFlowCreate, EnergyFlowUpdate]):
         results_max_timestamp = session.exec(max_timestamp_query).first()
         return results_min_timestamp, results_max_timestamp
 
-    def get_all_sorted_by_timestamp(
-        self, *, session: Session, limit: int = 0, offset: int = 0
-    ):
-        return session.exec(
-            select(EnergyFlow)
-            .order_by(asc(EnergyFlow.timestamp))
-            .limit(limit)
-            .offset(offset)
-        ).all()
-
     def get_sorted_by_timestamp(self, *, session: Session):
         return session.exec(
-            select(EnergyFlow).order_by(asc(EnergyFlow.timestamp))
+            select(EnergyFlow).order_by(EnergyFlow.timestamp.asc())
         ).first()
 
 
