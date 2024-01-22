70340739b414031d5fee5e337dba562d49d84d67
xlc-dev <>
2024-01-22 22:04:07 +0100

    remove runtime store here

--- frontend/src/components/sidebar.svelte
+++ frontend/src/components/sidebar.svelte
@@ -8,7 +8,7 @@
 
   import { onMount, createEventDispatcher } from "svelte";
 
-  import { stepperData, runtime } from "../lib/stores";
+  import { stepperData } from "../lib/stores";
 
   export let currentComponent: string;
 
@@ -18,9 +18,6 @@
 
   // Registers and handles button clicks by dispatching the corresponding event
   const handleButtonClick = (action: string) => {
-    if (action === "Stop") {
-      runtime.stop();
-    }
     dispatch("click", { action });
     currentComponent = action;
   };
