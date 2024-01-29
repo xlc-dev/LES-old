d83f0d0a7b0777cce9f1f7d24239d7ab36486122
xlc-dev <>
2024-01-29 20:05:26 +0100

    add seed option to seeder

--- backend/app/core/routers/seeder_router.py
+++ backend/app/core/routers/seeder_router.py
@@ -450,11 +450,14 @@ def create_household(
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model="None")
-def seed(session: Session = Depends(get_session)) -> None:
+def seed(
+    seed: int = random.random(), session: Session = Depends(get_session)
+) -> None:
     "Seeds the database with initial data for the twinworld. Deletes all previous in the database before seeding."  # noqa: E501
     delete_db_and_tables()
     create_db_and_tables()
-    random.seed()
+
+    random.seed(seed)
 
     energyflow = create_energyflow()
     for x in energyflow:
