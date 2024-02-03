f2672ccdbae94ecf0014ce5704b354f4bc2d79ad
Marvin Al Nashi <marvinalnashiedu@gmail.com>
2024-02-03 18:52:52 +0100

    Updated stepper step 0

--- frontend/src/components/stepper.svelte
+++ frontend/src/components/stepper.svelte
@@ -874,7 +874,7 @@
 
   {#if isStepZero}
     <div class="mx-auto pt-8">
-      <img src="/favicon.png" class="mx-auto w-48 rounded-lg" alt="LES Logo" />
+      <img src="/logo.png" class="mx-auto w-75 rounded-lg" alt="LES Logo" />
       <h1 class="pt-4 text-center text-4xl font-bold text-white">
         Local Energy System Simulation
       </h1>
@@ -887,6 +887,10 @@
         on:click={nextStep}
         class="block w-full rounded-lg bg-les-blue py-3 text-white transition duration-200 hover:brightness-110"
         >Get started</button>
+      <p class="py-4 text-center text-sm text-white">
+        This platform is developed for the UNESCO Chair of AI & DS for Society of the Hague University of Applied Sciences.
+        If you try this tool then please send any comments or feedback to Dr. Hani Alers: HAL@HHS.NL
+      </p>
     </div>
   {:else}
     {#each Object.keys(simulationData) as key, index (key)}
